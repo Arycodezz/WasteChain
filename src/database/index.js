8b1206590c709bb5da1f449d1efc727865c31ca9
Arycodezz <155063541+Arycodezz@users.noreply.github.com>
2025-07-17 14:07:13 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectToDB = async () => {
-    const connectionUrl = `mongodb+srv://ranjanaryan99:q6LvMVlhLwgKy2aD@cluster0.mhbvewc.mongodb.net/`;
+    const connectionUrl = `mongodb+srv://ranjanaryan99:${process.env.password}@cluster0.mhbvewc.mongodb.net/`;
     mongoose
         .connect(connectionUrl)
         .then(() => console.log("Connected to DB successfully"))
